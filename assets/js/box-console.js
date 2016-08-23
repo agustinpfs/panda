f7438c8975983656ea892d91f8926bda833761f7
agustinpfs <agustinpfs@gmail.com>
2016-08-22 22:51:45 -0300

    solved iframe embedded

--- assets/js/box-console.js
+++ assets/js/box-console.js
@@ -1,5 +1,7 @@
 document.querySelector('.post-content_button-console').onclick = function() {showConsole()};
+document.querySelector('.post-content_button-console2').onclick = function() {showConsole2()};
 document.querySelector('.post-content_buttonx-console').onclick = function() {hideConsole()};
+document.querySelector('.post-content_buttonx-console2').onclick = function() {hideConsole2()};
 
 function showConsole() {
     var x = document.querySelector('.post-content_console');
@@ -14,3 +16,19 @@ function hideConsole() {
 	var y = document.querySelector('.post-content_console');
 	y.style.display = 'none';
 }
+
+
+function showConsole2() {
+    var x = document.querySelector('.post-content_console2');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function hideConsole2() {
+	var y = document.querySelector('.post-content_console2');
+	y.style.display = 'none';
+}
+
