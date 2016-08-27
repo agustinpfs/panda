07ec457735c4397fbd651ca84f27fcf5cbcfb6f5
agustinpfs <agustinpfs@gmail.com>
2016-08-27 18:33:17 -0300

    testing

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET contact page. */
 router.get('/', function(req, res) {
     res.render('contact', { title: 'Contact Form' });
