5e4e3ee8e494ba5bccd9841a8e54d1dda079bcfa
agustinpfs <agustinpfs@gmail.com>
2016-11-09 18:23:46 -0300

    testing form

--- node-app-contact-form/routes/contact2.js
+++ node-app-contact-form/routes/contact2.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET about page. */
-router.get('/contact2', function(req, res) {
+router.get('/', function(req, res) {
     res.render('contact2');
 });
 
